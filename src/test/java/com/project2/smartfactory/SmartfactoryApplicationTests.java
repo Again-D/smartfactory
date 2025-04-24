99f17a990df318ae68ed71b7db51b5deb22fd20a
Again-D <nemcn5306@gmail.com>
2025-04-24 18:00:26 +0900

    Initialize Spring Boot project with Gradle setup, including application properties, main class, and test class.

---
+++ src/test/java/com/project2/smartfactory/SmartfactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project2.smartfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartfactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
