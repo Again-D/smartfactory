f89ea783b3d2bc0bf899919ba33d0ae37f1cc287
Again-D <nemcn5306@gmail.com>
2025-04-28 18:07:48 +0900

    Add user management features: implement DataNotFoundException, enhance UsersService with user retrieval and update methods, and create user edit and list templates.

--- src/main/java/com/project2/smartfactory/users/UsersController.java
+++ src/main/java/com/project2/smartfactory/users/UsersController.java
@@ -1,10 +1,15 @@
 package com.project2.smartfactory.users;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.validation.Valid;
@@ -33,6 +38,29 @@ public class UsersController {
         return "index";
     }
     
+    @GetMapping("/list")
+    public String usersList(Model model) { 
+        List<Users> usersList = this.usersService.getList();
+        model.addAttribute("usersList", usersList);
+        return "users_list";
+    }
+
+    @GetMapping(value = "/list/{id}")
+    public String showEditForm(Model model, @PathVariable("id") Integer id) {
+        Users user = this.usersService.getUser(id);
+        model.addAttribute("user", user);
+        return "user_edit";
+
+    }
+
+    @PutMapping("/list/{id}")
+    public String updateUser(@PathVariable("id") Integer id, @Valid UsersForm usersForm, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "user_edit";
+        }
+        this.usersService.updateUser(id,usersForm);
+        return "redirect:/users/list";
+    }
 
 
 
--- src/main/java/com/project2/smartfactory/users/UsersService.java
+++ src/main/java/com/project2/smartfactory/users/UsersService.java
@@ -2,9 +2,13 @@ package com.project2.smartfactory.users;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.project2.smartfactory.DataNotFoundException;
+
+import jakarta.transaction.Transactional;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +22,7 @@ public class UsersService {
         return this.usersRepository.findAll();
     }
 
+
     public void create(String userId, String password, String username, String email) {
         Users user = new Users();
         user.setUserId(userId);
@@ -26,6 +31,32 @@ public class UsersService {
         user.setEmail(email);
         user.setCreateDate(LocalDateTime.now());
         this.usersRepository.save(user);
+
+    }
+
+    public Users getUser(Integer id) {
+        Optional<Users> user = this.usersRepository.findById(id);
+        if (user.isPresent()) {
+            return user.get();
+        } else {
+            throw new DataNotFoundException("user not found");
+        }
+    }
+
+    @Transactional
+    public void updateUser(Integer id, UsersForm usersForm) {
+        Optional<Users> optionalUser = this.usersRepository.findById(id);
+
+        if (optionalUser.isPresent()) {
+            Users user = optionalUser.get();
+            user.setUserId(usersForm.getUserId());
+            user.setPassword(usersForm.getPassword());
+            user.setUsername(usersForm.getUsername());
+            user.setEmail(usersForm.getEmail());
+            this.usersRepository.save(user);
+        } else {
+            throw new RuntimeException(String.format("User not found with id: %s", id));
+        }
 
     }
 
